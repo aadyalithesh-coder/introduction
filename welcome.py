a0aaf2354d77638ec0341361e5dc814c8264d581
aadyalithesh-coder <aadyalithesh@gmail.com>
2026-02-23 17:01:56 +0100

    Add files via upload

---
+++ welcome.py
@@ -0,0 +1,10 @@
+print("Hello")
+print(5)
+number=2
+print(number)
+word="hi"
+print(word)
+print("the value of number is:",number)
+print("word is:",word)
+name1=input("what is your name:")
+print(name1)
