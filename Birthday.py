52e590cdf4df2058519827e3901eaf7b645291c6
aadyalithesh-coder <aadyalithesh@gmail.com>
2026-02-28 15:49:23 +0100

    Add files via upload

---
+++ Birthday.py
@@ -0,0 +1,14 @@
+Aadya="August 10"
+print(Aadya)
+
+Lithesh="March 30"
+print(Lithesh)
+
+Divya="September 12" 
+print(Divya)
+
+Dalvin="September 26"
+print(Dalvin)
+
+Stella="July 1"
+print(Stella)
