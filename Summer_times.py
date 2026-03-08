1ef5bdc8845ffa19aceecff6f69e27e5fc9f4d65
aadyalithesh-coder <aadyalithesh@gmail.com>
2026-03-08 19:50:34 +0100

    Add files via upload

---
+++ Summer_times.py
@@ -0,0 +1,7 @@
+temperature=float(input("Please enter the temperature:"))
+if temperature<=10:
+    print("wear warm clothes with jacket")
+if temperature>10:
+    print("wear a pullover")
+if temperature>=20:
+    print("wear short clothes")
