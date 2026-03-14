29754447a1dad300e080fa121f02cf4d60126d79
aadyalithesh-coder <aadyalithesh@gmail.com>
2026-03-14 17:56:09 +0100

    Add files via upload

---
+++ alphabet.py
@@ -0,0 +1,7 @@
+alpha=("a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z")
+character=(input("Enter character here:"))
+print(character)
+if character in alpha:
+    print("This character is in the alphabet")
+else:
+    print("This character is not in the alphabet")
